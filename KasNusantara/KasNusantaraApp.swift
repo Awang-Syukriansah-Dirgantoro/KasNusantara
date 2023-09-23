72d324e8b7f8adc43c5e1954ea9f756c60452c42
Awang-Syukriansah-Dirgantoro <awangsyukriansahdirgantoro@gmail.com>
2023-09-23 15:11:49 +0700

    Initial Commit

---
+++ KasNusantara/KasNusantaraApp.swift
@@ -0,0 +1,17 @@
+//
+//  KasNusantaraApp.swift
+//  KasNusantara
+//
+//  Created by Awang Syukriansah Dirgantoro on 23/09/23.
+//
+
+import SwiftUI
+
+@main
+struct KasNusantaraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
